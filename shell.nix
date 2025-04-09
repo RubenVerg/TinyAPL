c0fbccde0cb20473e21124c01aa36084cbeab84d
Amat <amatgilvinyes@gmail.com>
2025-04-09 22:45:00 +0200

    Make repo nix-development friendly (#5)
    * Make repo nix-development friendly
    
    * Minor typo
    
    * Fix shebangs
    
    * Add wasm compilation ability
    
    * Add deno, final flake.lock

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+  ghc-wasm-meta
+}:
+let
+  packages = with pkgs; [
+    # haskell part
+    haskell-language-server
+    haskell.compiler.ghc910
+    haskellPackages.Cabal_3_14_0_0
+    cabal-install
+
+    # C++ part
+    termcap
+    ncurses
+
+    # JS/wasm part
+    wasmtime
+    ghc-wasm-meta
+    typescript
+    deno
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
