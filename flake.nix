cacb9736af2a778df271bfbe1ccd7f99d07fed55
amatgil <amatgilvinyes@gmail.com>
2025-04-09 20:46:06 +0200

    Minor typo

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "a tiny APL dialect and interpreter in Haskell";
+  description = "A tiny APL dialect and interpreter in Haskell";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
