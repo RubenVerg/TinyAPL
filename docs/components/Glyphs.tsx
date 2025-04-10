3ea0b1cd30c4af13f59deb2db9efde9b7ec17acb
RubenVerg <RubenVerg@outlook.com>
2025-04-10 07:00:29 +0200

    Clickable glyphs in primitive page

--- docs/components/Glyphs.tsx
+++ docs/components/Glyphs.tsx
@@ -14,7 +14,7 @@ function Primitives({ pages }: GlyphsProps) {
 		<h1>Glyphs</h1>
 		
 		<ul>
-			{[...Object.entries(pages.glyphs)].sort(([_ak, av], [_bk, bv]) => av.name.localeCompare(bv.name)).map(([id, glyph]) => <li><a href={`/docs/glyph/${id}`}><code>{glyph.glyph}</code> {glyph.name}</a></li>)}
+			{[...Object.entries(pages.glyphs)].sort(([_ak, av], [_bk, bv]) => av.name.localeCompare(bv.name)).map(([id, glyph]) => <li><a class='link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href={`/docs/glyph/${id}`}><code>{glyph.glyph}</code> {glyph.name}</a></li>)}
 		</ul>
 	</>;
 }
